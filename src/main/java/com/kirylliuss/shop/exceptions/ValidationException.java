8bf0bbb90d43ffa3e5f7e59001d3206226e4055b
Klimashevich Kiryl <117638824+let-fughes@users.noreply.github.com>
2025-09-29 23:33:51 +0300

    Delete src directory

--- src/main/java/com/kirylliuss/shop/exceptions/ValidationException.java
+++
@@ -1,9 +0,0 @@
-package com.kirylliuss.shop.exceptions;
-
-import org.springframework.http.HttpStatus;
-
-public class ValidationException extends GlobalException {
-    public ValidationException(String message) {
-        super(message, HttpStatus.BAD_GATEWAY);
-    }
-}
